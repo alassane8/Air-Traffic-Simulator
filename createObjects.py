520065d8e6f1d92b073dc7ca7cbecd80c06e7193
alassane8 <alassane.wade@edu.ece.fr>
2026-04-30 21:30:57 +0200

    feat: create projects objects from data

---
+++ createObjects.py
@@ -0,0 +1,112 @@
+from aircraft import Aircraft
+from airport import Airport
+from terminal import Terminal
+from gate import Gate
+from runway import Runway
+
+
+def createAircrafts(aircraftsData: dict)-> Aircraft:
+    allAircrafts = {}
+
+    for aircraft in aircraftsData.get("aircrafts", []):
+        object = Aircraft(
+            aircraft.get("id", "NULL"),
+            aircraft.get("aircraft_code", "NULL"),
+            aircraft.get("seats", "NULL"),
+            aircraft.get("passengers", "NULL"),
+            aircraft.get("created_at", "NULL"),
+            aircraft.get("updated_at", "NULL")
+        )
+
+        allAircrafts[aircraft.get("id")] = object
+
+    return allAircrafts
+
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+def createAirports(airportsData: dict) -> Airport:
+    allAirports = {}
+
+    for airport in airportsData.get('airports', []):
+        object = Airport(
+            airport.get("id", "NULL"),
+            airport.get("airport_code", "NULL"),
+            airport.get("lat", "NULL"),
+            airport.get("long", "NULL"),
+            airport.get("created_at", "NULL"),
+            airport.get("updated_at", "NULL"),
+            airport.get("terminals", "NULL"),
+            airport.get("runways", "NULL"),
+            airport.get("gates", "NULL")
+        )
+
+        allAirports[airport.get("id")] = object
+
+    return allAirports
+
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+def createTerminals(airportsData: dict) -> Terminal:
+    allTerminals = {}
+
+    for airport in airportsData.get('airports', []):
+        for terminal in airport.get('terminals', []):
+            object = Terminal(
+                terminal.get("id", "NULL"),
+                terminal.get("terminal_code", "NULL"),
+                terminal.get("terminal_airport", "NULL"),
+                terminal.get("max_planes", "NULL"),
+                terminal.get("created_at", "NULL"),
+                terminal.get("updated_at", "NULL"),
+                terminal.get("planes_on_deck", "0"),
+                terminal.get("status", "OPEN")
+            )
+
+            allTerminals[terminal.get("id")] = object
+
+    return allTerminals
+
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+def createGates(airportsData: dict) -> Gate:
+    allGates = {}
+
+    for airport in airportsData.get('airports', []):
+        for gate in airport.get('terminals', []):
+            object = Terminal(
+                gate.get("id", "NULL"),
+                gate.get("gate_code", "NULL"),
+                gate.get("terminal", "NULL"),
+                gate.get("status", "FREE"),
+                gate.get("created_at", "NULL"),
+                gate.get("updated_at", "NULL")
+            )
+
+            allGates[gate.get("id")] = object
+
+    return allGates
+
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+def createRunways(airportsData: dict) -> Runway:
+    allRunways = {}
+
+    for airport in airportsData.get('airports', []):
+        for runway in airport.get('runways', []):
+            object = Runway(
+                runway.get("id", "NULL"),
+                runway.get("length", "NULL"),
+                runway.get("heading", "NULL"),
+                runway.get("current_aircraft", None),
+                runway.get("status", "FREE"),
+                runway.get("created_at", "NULL"),
+                runway.get("updated_at", "NULL")
+            )
+
+            allRunways[runway.get("id")] = object
+
+    return allRunways
