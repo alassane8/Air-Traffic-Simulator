fefeb5158a44a7bc224a9f3c412d3867d33bca6d
alassane8 <alassane.wade@edu.ece.fr>
2026-05-01 13:55:46 +0200

    fix: loadData compacted into one function

--- loadData.py
+++ loadData.py
@@ -1,22 +1,5 @@
 import json
 
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-def loadAirportsData() -> dict:
-    with open('airports.json', 'r') as file:
-        airports = json.load(file)
-    return airports
-
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-def loadAircraftData() -> dict:
-    with open('aircrafts.json', 'r') as file:
-        aircrafts = json.load(file)
-    return aircrafts
-
-""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-def loadedAirCorridorsData() -> dict:
-    with open('airCorridors.json', 'r') as file:
-        airCorridors = json.load(file)
-    return airCorridors
+def loadData(filename: str) -> dict:
+    with open(filename, 'r') as file:
+        return json.load(file)
