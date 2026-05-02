3e7de796225917f7e38fae450f9c7eecd66169b0
alassane8 <alassane.wade@edu.ece.fr>
2026-05-02 19:06:32 +0200

    fix: code lisibility

--- simulator.py
+++ simulator.py
@@ -1,11 +1,7 @@
 from datetime import datetime
 import random
-import uuid
-from gate import Gate, GateStatus
-from airCorridor import AirCorridor
-from runway import Runway
+from gate import GateStatus
 from terminal import TerminalStatus
-from flight import Flight, FlightStatus, FlightPriority
 
 def initializeAircraftsPositions(aircrafts: dict, gates: dict, terminals: dict) -> list:
     """"
@@ -13,7 +9,7 @@ def initializeAircraftsPositions(aircrafts: dict, gates: dict, terminals: dict) 
     """
     remaining_aircrafts = aircrafts
     remaining_gates = gates
-    occupied_gates = {}
+    occupied_gates = []
     
     while remaining_aircrafts:
         selected_aircraft = random.choice(list(remaining_aircrafts.values()))
@@ -29,7 +25,7 @@ def initializeAircraftsPositions(aircrafts: dict, gates: dict, terminals: dict) 
         selected_gate.status = GateStatus.OCCUPIED
         selected_gate.aircraft_id = selected_aircraft.id
         selected_gate.updated_at = datetime.now
-        occupied_gates = list.append(selected_gate)
+        occupied_gates.append(selected_gate)
 
         terminal_to_update = terminals[selected_gate.terminal]
 
@@ -42,57 +38,4 @@ def initializeAircraftsPositions(aircrafts: dict, gates: dict, terminals: dict) 
         del remaining_gates[selected_gate.id]
         del remaining_aircrafts[selected_aircraft.id]
 
-        return occupied_gates
-
-
-def flightInitialization(occupied_gates: list, terminals: dict):
-
-    for gate in occupied_gates:
-        new_flighzt = Flight(
-            id=uuid.uuid4(),
-            flight_code=initFlightCode(),
-            flight_status=initFlightStatus(),
-            aircraft_code=gate.aircraft_id,
-            priority=initFlightPriority(),
-            estimated_departure_time=initFlightDepartureTime(),
-            estimated_arrival_time=initFlightArrivalTime(),
-            departure_time=None,
-            arrival_time=None,
-            depart_airport_code=terminals[gate.terminal].airport_id,
-            depart_terminal_code=gate.terminal,
-            depart_gate_code=gate.gate_code,
-            depart_runway_code=initRunwayDeparture().id,
-            corridor_code=initCorridor().air_corridor_code,
-            arrival_airport_code=terminals[initGateArrival().terminal].airport_id,
-            arrival_terminal_code=terminals[initGateArrival().terminal].terminal_code,
-            arrival_gate_code=initGateArrival().gate_code,
-            arrival_runway_code=initRunwayArrival().id,
-)
-
-    # status planned, aircraft code, priority random avec + de proba sur normal
-    # choisir estimated en random
-def initFlightCode() -> str:
-
-
-def initFlightStatus() -> FlightStatus:
-
-
-def initFlightPriority() -> FlightPriority:
-
-
-def initFlightDepartureTime() -> datetime:
-
-
-def initFlightArrivalTime() -> datetime:
-
-
-def initRunwayDeparture() -> Runway:
-
-
-def initCorridor() -> AirCorridor:
-
-
-def initGateArrival() -> Gate:
-
-
-def initRunwayArrival() -> Runway:
+    return occupied_gates
