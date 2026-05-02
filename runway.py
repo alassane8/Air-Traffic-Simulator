add28a15d471b5281194913781b905f507f6e72e
alassane8 <alassane.wade@edu.ece.fr>
2026-05-02 19:05:42 +0200

    feat: adding RunwayStatus + adding new attributs

--- runway.py
+++ runway.py
@@ -1,15 +1,25 @@
 # models/runway.py
 from dataclasses import dataclass, field
-from typing import Optional
+from enum import Enum
+from typing import List, Optional
 from datetime import datetime
+from flight import Flight
 
+
+class RunwayStatus(str, Enum):
+    FREE = "FREE"
+    OCCUPIED = "OCCUPIED"
+    CLOSED = "CLOSED"
+    MAINTENANCE = "MAINTENANCE"
 
 @dataclass
 class Runway:
     id: str
+    airport_id: str
     length: float
     heading: int
-    status: str  # "FREE" | "OCCUPIED" | "CLOSED" | "MAINTENANCE"
+    status: RunwayStatus  # "FREE" | "OCCUPIED" | "CLOSED" | "MAINTENANCE"
     current_aircraft: Optional[str] = None
+    scheduled_flights: List[Flight] = field(default_factory=list)
     created_at: datetime = field(default_factory=datetime.now)
     updated_at: datetime = field(default_factory=datetime.now)
