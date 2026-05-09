454e9b4cb667fd68f22305ec182e6978c7fb92da
alassane8 <alassane.wade@edu.ece.fr>
2026-05-09 23:07:25 +0200

    feat: updated simulator

--- simulator.py
+++ simulator.py
@@ -4,44 +4,31 @@ from gate import GateStatus
 from terminal import TerminalStatus
 
 def assign_aircrafts_to_gates(aircrafts: dict, gates: dict, terminals: dict) -> list:
-    """
-    Random placements of aircrafts in terminal's gates
-
-    Args: 
-        aircrafts: aircraft dict
-        gates: gates dict
-        terminals: terminal dict
-    
-    Return: 
-        List of occupied gates by aircrafts
-
-    """
-    
-    unassigned_aircraft_ids = set(aircrafts.keys())
-    unassigned_gate_ids = set(gates.keys())
     occupied_gates = []
 
-    while unassigned_aircraft_ids:
+    unassigned_aircraft_ids = set(aircrafts.keys())
+    unassigned_gate_ids = set(gates.keys())
+
+    while unassigned_aircraft_ids and unassigned_gate_ids:
         aircraft_id = random.choice(list(unassigned_aircraft_ids))
         gate_id = random.choice(list(unassigned_gate_ids))
 
         selected_aircraft = aircrafts[aircraft_id]
         selected_gate = gates[gate_id]
 
-        if selected_aircraft.has_capacity():
-            selected_aircraft.load_passengers(random.randrange(selected_aircraft.seats))
-        else:
-            selected_aircraft.load_passengers(0)
-            
         terminal = terminals[selected_gate.terminal]
-        if terminal.can_accept_plane() and terminal.is_open():
-            terminal.add_plane()
-            selected_gate.assign_aircraft(selected_aircraft.id)
-            occupied_gates.append(selected_gate)
-
-        
 
         unassigned_aircraft_ids.discard(aircraft_id)
         unassigned_gate_ids.discard(gate_id)
+
+        if not terminal.can_accept_plane() or not terminal.is_open():
+            continue  # gate et avion perdus, mais au moins la boucle s'arrête proprement
+
+        if selected_aircraft.has_capacity():
+            selected_aircraft.load_passengers(random.randrange(selected_aircraft.seats))
+
+        terminal.add_plane()
+        selected_gate.assign_aircraft(selected_aircraft.id)
+        occupied_gates.append(selected_gate)
 
     return occupied_gates
